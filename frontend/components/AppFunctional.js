6622ce76727f7e13312d3245dac21301ad89150f
Zekereis <zekereisoglu@outlook.com>
2022-12-26 22:36:45 +0300

    ödev

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,73 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
 
 export default function AppFunctional(props) {
-  // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
-  // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
+  const initialState = [2,2]
+  const [konum , setKonum] = useState(initialState);
+  const [hamleSayisi,setHamleSayisi] = useState(0);
+  const [mesaj, setMesaj] = useState("");
 
-  function getXY() {
-    // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
-    // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
-  }
+  const konumAsIndex = (konum[1] -1) * 3 + konum[0] - 1;
 
-  function getXYMesaj() {
-    // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
-    // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
-    // tamamen oluşturulmuş stringi döndürür.
-  }
 
-  function reset() {
-    // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
-  }
+  function asagiGit() {
+    if(konum[1]<3){
+      setKonum ([konum[0],konum[1] + 1])
+      setHamleSayisi(hamleSayisi + 1);
+    }else{
+      setMesaj("Aşağı gidemezsiniz");
+    }
+  } 
 
-  function sonrakiIndex(yon) {
-    // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
-    // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
-    // şu anki indeksi değiştirmemeli.
-  }
+  function yukariGit() {
+    if(konum[1]>1){
+      setKonum ([konum[0],konum[1] - 1])
+      setHamleSayisi(hamleSayisi + 1);
+    }else{
+      setMesaj("Yukarı gidemezsiniz");
+    }
+  } 
+  
+  function sagaGit() {
+    if(konum[0]<3){
+      setKonum ([konum[0]+1,konum[1]])
+      setHamleSayisi(hamleSayisi + 1);
+    }else{
+      setMesaj("Sağa gidemezsiniz");
+    }
+  } 
 
-  function ilerle(evt) {
-    // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
-    // ve buna göre state i değiştirir.
-  }
+  function solaGit() {
+    if(konum[0]>1){
+      setKonum ([konum[0]-1,konum[1]])
+      setHamleSayisi(hamleSayisi + 1);
+    }else{
+      setMesaj("Sola gidemezsiniz");
+    }
+  } 
 
-  function onChange(evt) {
-    // inputun değerini güncellemek için bunu kullanabilirsiniz
-  }
-
-  function onSubmit(evt) {
-    // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+  function resetle() {
+    setKonum(initialState);
+    setHamleSayisi(0);
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">Koordinatlar ({konum.join(", ")})</h3>
+        <h3 id="steps">{hamleSayisi} kere ilerlediniz</h3>
       </div>
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+            <div key={idx} className={`square${idx === konumAsIndex ? ' active' : ''}`}>
+              {idx === konumAsIndex ? 'B' : null}
             </div>
           ))
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{mesaj}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={solaGit}>SOL</button>
+        <button id="up" onClick={yukariGit}>YUKARI</button>
+        <button id="right" onClick={sagaGit}>SAĞ</button>
+        <button id="down" onClick={asagiGit}>AŞAĞI</button>
+        <button id="reset" onClick={resetle}>reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="email girin"></input>
